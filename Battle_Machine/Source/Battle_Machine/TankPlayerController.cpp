c477d9e1ca6bf8a120a1b03550dafe31bbdb7d22
Dupegun <dupegun@gmail.com>
2018-02-05 21:55:40 +0300

    Add player controller (cpp)

---
+++ Battle_Machine/Source/Battle_Machine/TankPlayerController.cpp
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Tank.h"
+#include "TankPlayerController.h"
+
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Player controller not possesing a tank!"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Player controller possesing: %s"), *(ControlledTank->GetName()));
+	}
+}
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
